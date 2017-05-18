8c34b63b3e1d61bb65b07d18120094fc44188f27
CHAMARD Volodia <Volodia.chamard@formation.enjmin.fr>
2017-05-18 20:13:40 +0200

    Volo LD Skinning - Solution des pb de superposition de sprites et de normalMaps : MERCI THIBAULT <3 <3 <3

---
+++ Assets/Editor/RearrangeSortingOrderRecursively.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RearrangeSortingOrderRecursively : EditorWindow {
+	static int go_count = 0, replacing_count = 0;
+
+	public static float yStep = 0.01f;
+
+	private static int[] order;
+
+	[MenuItem ("Window/RearrangeSortingOrderRecursively")]
+	public static void ShowWindow () {
+		EditorWindow.GetWindow (typeof (RearrangeSortingOrderRecursively));
+	}
+
+	public void OnGUI () {
+		yStep = EditorGUILayout.FloatField ("Y step", yStep);
+		if (GUILayout.Button ("Replace sortingOrder in selected GameObjects")) {
+			FindInSelected ();
+		}
+	}
+	private static void FindInSelected () {
+		GameObject[] go = Selection.gameObjects;
+		order = new int[go.Length];
+		go_count = 0;
+		replacing_count = 0;
+		for (int i = 0;i < go.Length; i++) {
+			order[i] = 0;
+			FindInGO (go[i], i);
+		}
+		Debug.Log (string.Format ("Searched {0} GameObjects, replaced sortingOrder on {1} objects", go_count, replacing_count));
+	}
+
+	private static void FindInGO (GameObject g, int index) {
+		go_count++;
+		SpriteRenderer sr = g.GetComponent<SpriteRenderer> ();
+		if (sr) {
+			sr.sortingOrder = order[index];
+			g.transform.localPosition = new Vector3 (g.transform.localPosition.x, order[index] * yStep, g.transform.localPosition.z);
+			g.transform.parent.localPosition = new Vector3 (g.transform.parent.localPosition.x, 0.0f, g.transform.parent.localPosition.z);
+			order[index]++;
+			replacing_count++;
+			Debug.Log ("Resorted on object : " + g);
+		}
+		// Now recurse through each child GO (if there are any):
+		foreach (Transform childT in g.transform) {
+			//Debug.Log("Searching " + childT.name  + " " );
+			FindInGO (childT.gameObject, index);
+		}
+	}
+}
