2c677d357d5d51aab9fe59a919a78c778fdc3938
Dioptre <misiak.benjamin@gmail.com>
2017-05-30 15:34:11 +0200

    alternance lamp

--- Assets/script/lampFireflySpxn.cs
+++ Assets/script/lampFireflySpxn.cs
@@ -89,17 +89,14 @@ public class lampFireflySpxn : MonoBehaviour {
 			switch (waveNumber) {
 				case 0:
 					fireflyColor = Color.cyan;
-					waveNumber++;
 					break;
 
 				case 1:
 					fireflyColor = Color.yellow;
-					waveNumber++;
 					break;
 
 				case 2:
 					fireflyColor = Color.magenta;
-					waveNumber = 0;
 					break;
 			}
 
@@ -135,6 +132,9 @@ public class lampFireflySpxn : MonoBehaviour {
 				if (Vector3.Distance (this.transform.position, lampe.transform.position) < distanceActivationLampe) {
 					if (lampe.GetComponentInChildren<Light> ().intensity == 0) {
 						lampeAllume++;
+						Debug.Log (waveNumber);
+						waveNumber = (waveNumber+1)%3;
+						Debug.Log (waveNumber);
 						this.transform.GetChild (0).GetComponent<Light> ().intensity -= intensityOfLampist/totalNbrLampes;
 
 						if (lampeAllume == totalNbrLampes)
