f2578c944128fc6f8d8355e020537df70c514c4c
Dioptre <misiak.benjamin@gmail.com>
2017-06-02 16:28:14 +0200

    big light

--- Assets/script/bigLight.cs
+++ Assets/script/bigLight.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bigLight : MonoBehaviour {
+public class bigLight : Lookable {
 
-	// Use this for initialization
-	void Start () {
-		
+	bool charging;
+	public float croissance = 0.01f;
+	public float speedOfLoss = 2;
+
+	protected override void StartLookable ()
+	{
+		base.StartLookable ();
+		charging = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public override void DoAction ()
+	{
 		
+		base.DoAction ();
+		charging = true;
+	}
+
+	public override void QuitSee ()
+	{
+		base.QuitSee ();
+		charging = false;
+	}
+
+	protected override void UpdateLookable ()
+	{
+		base.UpdateLookable ();
+		Debug.Log (conditionsDactivation.Length);
+		if (charging)
+		{
+			this.transform.localScale = new Vector3(transform.localScale.x+croissance, 1, transform.localScale.z + croissance);
+			this.GetComponentInChildren<Light> ().intensity += croissance;
+		}
+		else
+		{
+			if (transform.localScale.x > 2)
+			{
+				this.transform.localScale = new Vector3 (transform.localScale.x - croissance/2, 1, transform.localScale.z - croissance/2);
+			}
+		}
 	}
 }
