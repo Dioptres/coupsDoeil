eb87dafdd9ae7bb4587f6145803e59250cb2406a
Dioptre <Jean.nicolas@formation.enjmin.fr>
2017-06-08 17:07:07 +0200

    push d'avant de quitter l'école.

---
+++ Assets/script/letThemCome.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class letThemCome : MonoBehaviour {
+
+	UnityEngine.AI.NavMeshAgent agent;
+
+
+
+	// Use this for initialization
+	void Start () {
+		agent = GameObject.Find ("Bassiste").GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+/*
+batteur : -4.5_2.6
+bassiste : -3.6_-1.2
+flutiste : -1.1_-1.8
+guitariste : -1.5_2.2
+Ukulele : 0.2_1.1
+*/
