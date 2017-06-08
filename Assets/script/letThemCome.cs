4f46662595aa49103947445d732b6952c09e2835
Dioptre <Jean.nicolas@formation.enjmin.fr>
2017-06-08 17:14:54 +0200

    bis

--- Assets/script/letThemCome.cs
+++ Assets/script/letThemCome.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class letThemCome : MonoBehaviour {
 
 	UnityEngine.AI.NavMeshAgent agent;
-
+	Animator anim;
 
 
 	// Use this for initialization
 	void Start () {
 		agent = GameObject.Find ("Bassiste").GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		agent.destination = new Vector3 (-3.6f,0,-1.2f);
+
+		anim = GameObject.Find ("Bassiste").GetComponentInChildren<Animator> ();
+		anim.SetBool ("isWalking", true);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		Debug.Log (agent.remainingDistance);
+		if(agent.remainingDistance == 0)
+		{
+			anim.SetBool ("isWalking", false);
+		}
 	}
 }
 
 
 /*
-batteur : -4.5_2.6
 bassiste : -3.6_-1.2
 flutiste : -1.1_-1.8
 guitariste : -1.5_2.2
