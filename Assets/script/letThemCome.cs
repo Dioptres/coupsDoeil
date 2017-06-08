832788fe25a619c70b6a29b3f3de0e38ecf50301
Dioptre <misiak.benjamin@gmail.com>
2017-06-08 21:16:48 +0200

    musicians come in

--- Assets/script/letThemCome.cs
+++ Assets/script/letThemCome.cs
@@ -6,15 +6,21 @@ public class letThemCome : MonoBehaviour {
 
 	UnityEngine.AI.NavMeshAgent agent;
 	Animator anim;
+	Animator animLune;
+
+	int index;
 
 
 	// Use this for initialization
 	void Start () {
+		index = 0;
+		animLune = GetComponentInChildren<Animator> ();
 		agent = GameObject.Find ("Bassiste").GetComponent<UnityEngine.AI.NavMeshAgent> ();
 		agent.destination = new Vector3 (-3.6f,0,-1.2f);
 
 		anim = GameObject.Find ("Bassiste").GetComponentInChildren<Animator> ();
 		anim.SetBool ("isWalking", true);
+		animLune.SetInteger ("moonColor", 1);
 	}
 	
 	// Update is called once per frame
@@ -24,14 +30,43 @@ public class letThemCome : MonoBehaviour {
 		if(agent.remainingDistance == 0)
 		{
 			anim.SetBool ("isWalking", false);
+			if (index == 0)
+			{
+				index++;
+				agent = GameObject.Find ("Flûtiste").GetComponent<UnityEngine.AI.NavMeshAgent> ();
+				agent.destination = new Vector3 (-1.1f, 0, -1.8f);
+
+				anim = GameObject.Find ("Flûtiste").GetComponentInChildren<Animator> ();
+				anim.SetBool ("isWalking", true);
+				animLune.SetInteger ("moonColor", 2);
+			}
+			else if (index == 1)
+			{
+				index++;
+				agent = GameObject.Find ("Guitariste").GetComponent<UnityEngine.AI.NavMeshAgent> ();
+				agent.destination = new Vector3 (-1.5f, 0, 2.2f);
+
+				anim = GameObject.Find ("Guitariste").GetComponentInChildren<Animator> ();
+				anim.SetBool ("isWalking", true);
+				animLune.SetInteger ("moonColor", 4);
+			}
+			else if (index == 2)
+			{
+				index++;
+				agent = GameObject.Find ("Ukulele").GetComponent<UnityEngine.AI.NavMeshAgent> ();
+				agent.destination = new Vector3 (0.2f, 0, 1.1f);
+
+				anim = GameObject.Find ("Ukulele").GetComponentInChildren<Animator> ();
+				anim.SetBool ("isWalking", true);
+				animLune.SetInteger ("moonColor", 3);
+			}
+			else if (index == 3)
+			{
+				animLune.SetInteger ("moonColor", 0);
+				this.GetComponent<singerSimon> ().enabled = true;
+				this.GetComponent<letThemCome> ().enabled = false;
+			}
 		}
 	}
 }
 
-
-/*
-bassiste : -3.6_-1.2
-flutiste : -1.1_-1.8
-guitariste : -1.5_2.2
-Ukulele : 0.2_1.1
-*/
