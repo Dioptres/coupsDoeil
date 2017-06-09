f2d7f510a7530538ac86afec32f7a11a843cbac3
Dioptre <misiak.benjamin@gmail.com>
2017-06-09 21:34:21 +0200

    destroy Firefly on lampist scene after 1s

---
+++ Assets/script/destroyOnTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnTime : MonoBehaviour {
+
+	public float timer = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer <= 0)
+		{
+			Destroy (this.gameObject);
+		}
+
+		timer -= Time.deltaTime;
+	}
+}
